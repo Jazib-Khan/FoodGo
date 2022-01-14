ae5b9241f777fc112f3221fbf1ccd03a8b384554
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2022-01-14 02:02:58 +0000

    Edit button implemented
    User can edit food items and change the title, description and markdown

--- routes/foods.js
+++ routes/foods.js
@@ -6,30 +6,46 @@ router.get('/new', (req, res) => {
     res.render('foods/new', { food: new Food() })
 })
 
+router.get('/edit/:id', async(req, res) => {
+    const food = await Food.findById(req.params.id)
+    res.render('foods/edit', { food: food })
+})
+
 router.get('/:slug', async (req, res) => {
     const food = await Food.findOne({ slug: req.params.slug })
     if (food == null) res.redirect('/')
     res.render('foods/show', { food: food })
 })
 
-router.post('/', async (req, res) => {
-    let food = new Food({
-        title: req.body.title,
-        description: req.body.description,
-        markdown: req.body.markdown
-    })
-    try {
-        food = await food.save()
-        res.redirect(`/foods/${food.slug}`)
-    } catch (e) {
-        console.log(e)
-        res.render(`foods/new`, { food: food})
-    }  
-})
+router.post('/', async (req, res, next) => {
+    req.food = new Food()
+    next()
+}, saveFoodAndRedirect('new'))
+
+router.put('/:id', async (req, res, next) => {
+    req.food = await Food.findById(req.params.id)
+    next()
+}, saveFoodAndRedirect('edit'))
 
 router.delete('/:id', async (req, res) => {
     await Food.findByIdAndDelete(req.params.id)
     res.redirect('/')
 })
+
+function saveFoodAndRedirect(path) {
+    return async (req, res) => {
+        let food = req.food
+        food.title = req.body.title
+        food.description = req.body.description
+        food.markdown = req.body.markdown
+        try {
+            food = await food.save()
+            res.redirect(`/foods/${food.slug}`)
+        } catch (e) {
+            console.log(e)
+            res.render(`foods/${path}`, { food: food })
+        }  
+    }
+}
 
 module.exports = router 
