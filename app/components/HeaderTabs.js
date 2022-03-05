9c93d9576abed21ddaf0f752329f5f72d1f37c84
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2022-03-05 15:59:42 +0000

    Further stylised header tabs
    Added alternating colours on header tabs for UX experience for the user to know which tab is selected.

--- app/components/HeaderTabs.js
+++ app/components/HeaderTabs.js
@@ -1,22 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TouchableOpacity} from 'react-native';
 
 export default function HeaderTabs() {
+        const [activeTab, setActiveTab] = useState("Delivery");
     return (
-        <View style={{flexDirection: "row", alignSelf: "center" }}>
-            <HeaderButton text="Delivery"/>
-            <HeaderButton text="Pickup"/>
+        <View style={{flexDirection: "row", alignSelf: "center", paddingVertical: 36 }}>
+            <HeaderButton 
+            text="Delivery" 
+            backgroundColor="black" 
+            color="white" 
+            activeTab={activeTab} 
+            setActiveTab={setActiveTab}
+            />
+            <HeaderButton 
+            text="Pickup" 
+            backgroundColor="white" 
+            color="black" 
+            activeTab={activeTab} 
+            setActiveTab={setActiveTab}
+            />           
         </View>
     );
 }
 
 const HeaderButton = (props) => (
-    <TouchableOpacity style={{
-        backgroundColor: "black",
-        paddingVertical: 6,
-        paddingHorizontal: 16,
-        borderRadius: 30,
-    }}>
-        <Text style={{ color: "white"}}>{props.text}</Text>
+    <TouchableOpacity 
+        style={{
+            backgroundColor: props.activeTab === props.text ? "black" : "white",
+            paddingVertical: 6,
+            paddingHorizontal: 16,
+            borderRadius: 30,
+        }}
+        onPress = {() => props.setActiveTab(props.text)}
+    >
+        <Text 
+            style={{ 
+                color: props.activeTab === props.text ? "white" : "black", 
+                fontSize: 15, 
+                fontWeight: "900",
+            }}
+        >
+            {props.text}
+        </Text>
     </TouchableOpacity>
 );
